a0772fb36d0bd49ceb30de5b283641dc967940dc
ashutoshdas18@gmail.com <ashutoshdas18@gmail.com>
2023-04-05 10:34:17 +0530

    Added notifications for payment, emi. Added chat options from borrower's dashboard

---
+++ product-webapp/lendinglane/src/app/lender/lender-transaction/lender-transaction.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lender-transaction',
+  templateUrl: './lender-transaction.component.html',
+  styleUrls: ['./lender-transaction.component.css']
+})
+export class LenderTransactionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
