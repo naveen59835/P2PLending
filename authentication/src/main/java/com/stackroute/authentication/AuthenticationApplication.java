4471c2dafb67ccc172bbfde63ee9cb2dc968eb1f
Prashu <Prashanth.P@stackroute.in>
2023-04-18 15:45:37 +0530

    Eureka changes

--- authentication/src/main/java/com/stackroute/authentication/AuthenticationApplication.java
+++ authentication/src/main/java/com/stackroute/authentication/AuthenticationApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableFeignClients
 public class AuthenticationApplication {
 
